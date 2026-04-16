b8e54e0c63c2ba7dda65fc2055fbcba0bbaff873
CRISTIAN ANDRE FAGUNDES <5791232@uninteracad.local>
2026-04-16 20:31:20 -0300

    apagar

---
+++ ciris.py
@@ -0,0 +1,9 @@
+nomes = ['cristian','daniele','vinnicius','floco']
+for n in nomes:
+    print(n)
+    
+    for i in range(0,6,1):
+     print(i)
+
+     for i in range(5):
+      print(i)
---
+++ uninter.py
@@ -0,0 +1,4 @@
+lista_numeros = [1,2,3,4,6,8,-3,-4,92929]
+
+for x in lista_numeros = []
+print(lista_numeros)
